5dace755fa179f86b58f1365c314458fd0f80c60
Alana Nanz <97478674+alana-nanz@users.noreply.github.com>
2022-02-28 14:30:19 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NameTag from "./NameTag.js";
+import "./styles.css";
+
+const App = () => (
+  <div className="App">
+    <h1>Name Tag Generator</h1>
+    <NameTag name="Alana" />
+    <NameTag name="Eric" />
+    <NameTag name="Amelia" />
+    <NameTag name="Zoe" />
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
